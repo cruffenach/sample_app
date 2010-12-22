eab94d3bb999c7d297675e00e8d6dc8aaa7af461
Collin Ruffenach <cruffenach@gmail.com>
2010-12-22 14:41:39 -0700

    Added gravatar in

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.3'
 
 gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'gravatar_image_tag', '0.1.0'
 
 group :development do
   gem 'rspec-rails', '2.2.0'
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Collin Ruffenach"
+  user.email                 "cruffenach@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
