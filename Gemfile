0faee2fe40a8952ecef907ecfa0d9bd270f8e870
Collin Ruffenach <cruffenach@gmail.com>
2010-12-22 14:16:51 -0700

    Added a user factory and test for show page

--- Gemfile
+++ Gemfile
@@ -12,4 +12,5 @@ end
 group :test do
 	gem 'rspec', '2.2.0'
 	gem 'webrat', '0.7.2'
+	gem 'factory_girl_rails', '1.0'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
   resources :users
 
   root :to => "pages#home"
+  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
